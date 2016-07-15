0c5bd39635a79bf2b428652d74e4949462206e87
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-07-15 14:29:05 -0300

    Added profile page
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- static/js/clouds.js
+++ static/js/clouds.js
@@ -1,29 +1,17 @@
 var fill = function(i) {
     tones = ['#A3A3A3', '#818181', '#606060', '#4A4A4A', '#2B2B2B']
-    return tones[i%5];
+    // return tones[i%5];
+    var index = Math.floor(Math.random() * tones.length);
+    return tones[index];
 }
+var fill = d3.scale.category20();
 
 var w = $(".word-cloud").width(),
     h = $(".word-cloud").height();
 
-var speechLayout = d3.layout.cloud()
-    .size([w, h])
-    .words([
-      "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this"].map(function(d) {
-      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-    }))
-    .padding(10)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Lato")
-    .fontSize(function(d) { return d.size; })
-    .on("end", drawSpeechCloud);
-
-speechLayout.start();
+var scaleWords = function(size) {
+  return (size * 7) + 'px';
+}
 
 function drawSpeechCloud(words) {
   d3.select("#speechCloud").style('width', w).style('height', h)
@@ -35,34 +23,16 @@ function drawSpeechCloud(words) {
     .selectAll("text")
       .data(words)
     .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-size", function(d) { return scaleWords(d.size) })
       .style("font-family", "Lato")
       .style("fill", function(d, i) { return fill(i); })
+      // .style('padding', '1rem')
       .attr("text-anchor", "middle")
       .attr("transform", function(d) {
         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
       })
       .text(function(d) { return d.text; });
 }
-
-var proposalLayout = d3.layout.cloud()
-    .size([w, h])
-    .words([
-      "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this", "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this"].map(function(d) {
-      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-    }))
-    .padding(10)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Lato")
-    .fontSize(function(d) { return d.size; })
-    .on("end", drawProposalCloud);
-
-proposalLayout.start();
 
 function drawProposalCloud(words) {
   d3.select("#proposalCloud").style('width', w).style('height', h)
@@ -74,7 +44,7 @@ function drawProposalCloud(words) {
     .selectAll("text")
       .data(words)
     .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-size", function(d) { return scaleWords(d.size) })
       .style("font-family", "Lato")
       .style("fill", function(d, i) { return fill(i); })
       .attr("text-anchor", "middle")
